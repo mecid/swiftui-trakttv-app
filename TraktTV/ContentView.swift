2b2eec91f04b60ae398ce64d87eaad695117fdf6
Majid Jabrayilov <cmecid@gmail.com>
2020-04-20 16:56:50 +0400

    Initial Commit

---
+++ TraktTV/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TraktTV
+//
+//  Created by Majid Jabrayilov on 4/20/20.
+//  Copyright © 2020 Majid Jabrayilov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
