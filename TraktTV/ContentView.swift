0dab26bad5fdf27b7efe873b319993dfe3dad8ed
Majid Jabrayilov <cmecid@gmail.com>
2020-04-21 16:38:22 +0400

    Root view implemented

--- TraktTV/ContentView.swift
+++ TraktTV/ContentView.swift
@@ -5,12 +5,25 @@
 //  Created by Majid Jabrayilov on 4/20/20.
 //  Copyright © 2020 Majid Jabrayilov. All rights reserved.
 //
-
 import SwiftUI
+import KingfisherSwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var store: SearchStore
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(SearchType.allCases, id: \.self) { type in
+                        ShelfView(
+                            title: type.rawValue.capitalized,
+                            shows: self.store.listOfShows(for: type)
+                        )
+                    }
+                }
+            }.onAppear(perform: store.fetch)
+        }
     }
 }
 
